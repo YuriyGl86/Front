690b67baecbee77b709badf181da3f3b770f71fe
YuriyGl86 <yuriygl86@gmail.com>
2023-09-06 17:01:15 +0300

    final

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,22 @@
-import Ticket from "./Ticket"
+import Ticket from './Ticket';
 
-const ticket = new Ticket(document.querySelector('.container'))
+const ticket = new Ticket(document.querySelector('.container'));
+ticket.init();
 
-const form = document.querySelector('.add-ticket-form')
-const addTicketCancelButton = document.querySelector('.add-form-cancel-button')
+// const form = document.querySelector('.add-ticket-form')
 
-addTicketCancelButton.addEventListener('click', (event) => {
-    event.target.closest('.modalBackground').classList.remove('show')
-})
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault()
 
+//     fetch('http://localhost:7071', {
+//         method: 'POST',
+//         body: new FormData(form),
+//     }).then(response => response.json())
+//     .then(data => {
+//         console.log(data.id)
+//         ticket.renderTicket(data)
+//     })
 
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    fetch('http://localhost:7071', {
-        method: 'POST',
-        body: new FormData(form),
-    }).then(response => response.json())
-    .then(data => {
-        console.log(data.id)
-        ticket.renderTicket(data)
-    })
-
-    form.reset()
-    form.closest('.modalBackground').classList.remove('show')
-})
+//     form.reset()
+//     form.closest('.modalBackground').classList.remove('show')
+// })
